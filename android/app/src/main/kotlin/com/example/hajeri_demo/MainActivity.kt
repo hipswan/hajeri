951463d8412b5d7a8d1982c522d0c0d5b480beca
Atul Singh <46633779+hipswan@users.noreply.github.com>
2021-05-03 23:27:58 +0530

    release plus firebase configuration

--- android/app/src/main/kotlin/com/example/hajeri_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hajeri_demo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.hajeri_demo
+package am.attendance.hajeri
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.android.FlutterFragmentActivity
